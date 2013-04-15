a2711f77d461f4ad0dd6016a2981e2759c60af2f
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-15 14:55:03 -0300

    print results at the end

--- src/html2swf/main.clj
+++ src/html2swf/main.clj
@@ -4,14 +4,24 @@
   (:use [clojure.tools.cli :only [cli]]))
 
 ;;rotos:
-;;108 -> color de fondo text => no parsea p:first-entry
 ;;120 -> image va abajo quedo a la izquierda
-;;110 -> falta imagen, podria ser mas chico
+;;110, 4 -> falta imagen, podria ser mas chico
 ;;119 -> falta raya abajo
+;;115 -> imagen va arriba
+;;114 y 116 no parsea la imagen del gradiente loco
+;;103 la imagen quedo en cualquier lado
+;;102 -> faltan las imagenes del cabezal de mail
+;;3 -> falta la rayita abajo
+;;112 -> imagen un poco corrida 
+;;101 -> falta la imagen sobre la derecha del satelite que antes estaba
 
 (defn -main
   "Application entry point, should receive the directory to compile"
   [& args]
   (let [[options args banner] (cli args
                      ["-d" "--directory" "Directory where files to be built are located" :default "."])]
-    (compiler/build-directory (:directory options))))
+    (let [result (compiler/build-directory (:directory options))]
+      (println (format "Compiled %s files %s success %s failed"
+                       (count result)
+                       (count (filter true? result))
+                       (count (filter false? result)))))))
