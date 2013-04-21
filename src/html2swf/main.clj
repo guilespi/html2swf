56f72fa047c6e28adcb9770d74e485fa75f9f2d6
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-21 17:34:30 -0300

    removed todo list from main file

--- src/html2swf/main.clj
+++ src/html2swf/main.clj
@@ -3,18 +3,6 @@
   (:require [html2swf.compiler :as compiler])
   (:use [clojure.tools.cli :only [cli]]))
 
-;;rotos:
-;;120 -> image va abajo quedo a la izquierda
-;;110, 4 -> falta imagen, podria ser mas chico
-;;119 -> falta raya abajo
-;;115 -> imagen va arriba
-;;114 y 116 no parsea la imagen del gradiente loco
-;;103 la imagen quedo en cualquier lado
-;;102 -> faltan las imagenes del cabezal de mail
-;;3 -> falta la rayita abajo
-;;112 -> imagen un poco corrida 
-;;101 -> falta la imagen sobre la derecha del satelite que antes estaba
-
 (defn -main
   "Application entry point, should receive the directory to compile"
   [& args]
